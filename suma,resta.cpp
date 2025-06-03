5d34a1829396387047be19ff2ac9ef43e7b8b36c
sebas0gl <sebastiangutierrezlopez2007@gmail.com>
2025-06-03 17:55:28 -0600

    se modifico eel dknevoi

--- suma,resta.cpp
+++ suma,resta.cpp
@@ -21,32 +21,7 @@ while (true){
         cout << "ingrese el segundo número: ";
         cin >> num2; 
 
-        switch (opcion){
-            case 1:
-            resultado = num1 + num2;
-            cout << "resultado: "  << resultado << endl;
-            break;
-            case 2:
-            resultado = num1 - num2;
-            cout << "resultado: " << resultado << endl;
-            break;
-            case 3:
-            resultado = num1 * num2;
-            cout << "resultado: " << resultado << endl;
-            break;
-            case 4:
-            if (num2 != 0){
-                resultado = num1 / num2;
-                cout << "resultado: " << resultado << endl;
-            } else {
-                cout << "error: no se puede dividir por cero." << endl;
-            }
-            break;
-        }
-        break; // salir del bucle si la opcion fue valida y ejecutada
-    } else {
-        cout << "opción inválida. intente de nuevo. " << endl;
-    }
+        
 }
 return 0;
 }
